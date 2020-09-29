4436335e83f7341449a4becaaccd86796e8b818e
Jeong Hye-mi <hyami222@gmail.com>
2020-09-29 16:36:14 +0900

    Update mani_ecat_profile_pos.cpp

--- ethercat_test/src/mani_ecat_profile_pos.cpp
+++ ethercat_test/src/mani_ecat_profile_pos.cpp
@@ -112,7 +112,7 @@ boolean ecat_init(uint32_t mode)
                     wkc_count=ec_SDOwrite(k+1, 0x1A00, 0x03, FALSE, os, &ob, EC_TIMEOUTRXM);
                     // 4. DigitalInput(LogicState) UINT16
                     os=sizeof(ob); ob = 0x60FD0020; // Digital Inputs
-                    wkc_count=ec_SDOwrite(k+1, 0x1A00, 0x03, FALSE, os, &ob, EC_TIMEOUTRXM);
+                    wkc_count=ec_SDOwrite(k+1, 0x1A00, 0x04, FALSE, os, &ob, EC_TIMEOUTRXM);
                     // 5. ErrorCode UINT16
                     os=sizeof(ob); ob = 0x603F0010;
                     wkc_count=ec_SDOwrite(k+1, 0x1A00, 0x05, FALSE, os, &ob, EC_TIMEOUTRXM);
